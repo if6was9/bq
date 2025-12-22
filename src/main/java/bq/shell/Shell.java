7262268885c14d09bfaa1771652e28cccf896ebd
Rob Schoening <robschoening@gmail.com>
2025-12-22 10:56:40 -0800

    basic repl

--- src/main/java/bq/shell/Shell.java
+++ src/main/java/bq/shell/Shell.java
@@ -1,13 +1,8 @@
 package bq.shell;
 
-import com.google.common.io.Files;
-import java.io.File;
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
 import javax.script.ScriptException;
-import jdk.jshell.JShell;
 
 public class Shell {
 
@@ -15,29 +10,7 @@ public class Shell {
     // TODO Auto-generated constructor stub
   }
 
-  public static void main(String[] args) throws IOException {
-    JShell shell = JShell.builder().executionEngine("local").build();
-
-    String init =
-        Files.asCharSource(new File("./src/main/resources/bqsh/init.jsh"), StandardCharsets.UTF_8)
-            .read();
-
-    shell.eval(init);
-
-    if (args.length != 1) {
-      usage();
-    }
-    File scriptFile = new File(args[0]);
-
-    String script = Files.asCharSource(scriptFile, StandardCharsets.UTF_8).read();
-
-    shell.eval(script);
-  }
-
-  static void usage() {
-    System.err.println("Usage: Shell <jsh script>");
-    System.exit(1);
-  }
+  // NOTE: the out-of-process entrypoint for running scripts is bqsh which invokes jshell
 
   public void test() {
 
