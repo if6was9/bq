ac7f283fd65e31eb0f2c4715d2762a6bbba035f8
Rob Schoening <robschoening@gmail.com>
2025-12-26 14:20:10 -0800

    add slightly more advanced sample script test

--- src/test/java/bq/util/ScriptTest.java
+++ src/test/java/bq/util/ScriptTest.java
@@ -1,10 +1,12 @@
 package bq.util;
 
+import javax.script.Bindings;
 import javax.script.Compilable;
 import javax.script.CompiledScript;
 import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
 import javax.script.ScriptException;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class ScriptTest {
@@ -43,16 +45,28 @@ public class ScriptTest {
 
       String script =
           """
-            public class Script {
-                public String getMessage() {
-                	return "Hello World";
-                }
-            }
+          package foo;
+              public class Script {
+
+
+          public String abc =  "abc";
+                  public String foo() {
+                  System.out.println(getClass());
+                  abc="def";
+                  	return "Hello World";
+                  }
+              }
 
           """;
 
+      Bindings bindings = engine.createBindings();
+      bindings.put("abc", "Hello world");
+
       CompiledScript compiledScript = compiler.compile(script);
-      Object result = compiledScript.eval();
+      Object result = compiledScript.eval(bindings);
+
+      Assertions.assertThat(bindings.get("abc")).isEqualTo("def");
+
       System.out.println("Result: " + result);
     } catch (ScriptException e) {
       e.printStackTrace();
