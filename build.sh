9f6d921dcf1f99b9ce2f443350fecc0a8773567a
Rob Schoening <robschoening@gmail.com>
2025-12-29 20:51:11 -0800

    branch name

--- build.sh
+++ build.sh
@@ -14,7 +14,8 @@ fi
 
 if [[ -z "${SKIP_DOCKER}" ]]; then
 if [[ "${CI}" = "true" ]]; then
-docker buildx build .  --platform linux/amd64,linux/arm64 --push --tag ghcr.io/if6was9/bq:latest
+IMAGE_TAG_NAME=${GITHUB_REF}
+docker buildx build .  --platform linux/amd64,linux/arm64 --push --tag ghcr.io/if6was9/bq:${IMAGE_TAG_NAME}
 else
 docker buildx build .  --platform linux/arm64,linux/amd64 --tag ghcr.io/if6was9/bq:latest
 fi
